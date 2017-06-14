0082dee28812c371e8608d16cf1767503ea9445c
Osku Sirpoma <osku.sirpoma@gmail.com>
2017-06-14 22:07:06 +0300

    -

--- p5Projekcts/Snake/p5/empty-example/sketch.js
+++ p5Projekcts/Snake/p5/empty-example/sketch.js
@@ -1,7 +1,41 @@
+var s;
+var scl = 20;
+var food;
+
 function setup() {
+  createCanvas(600,600);
+  s = new Snake();
+  frameRate(10);
+  //food = createVector(random(width), random(height));
 
 }
 
+function pickLocation() {
+  var cols = floor(width/scl);
+  var rows = floor(height/scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+}
+
 function draw() {
-  
+  background(51);
+  s.update();
+  s.show();
+
+  s.eat(food);
+
+  fill(255, 0, 0);
+  rect(food.x, food.y, scl, scl);
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  }
 }
