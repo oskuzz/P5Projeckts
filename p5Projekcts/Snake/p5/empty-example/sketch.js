5100ac5eb61a41fe11b97f41857fe46df6cbf3ed
Osku Sirpoma <osku.sirpoma@gmail.com>
2017-06-14 23:36:58 +0300

    -

--- p5Projekcts/Snake/p5/empty-example/sketch.js
+++ p5Projekcts/Snake/p5/empty-example/sketch.js
@@ -1,41 +1,65 @@
 var s;
 var scl = 20;
 var food;
+var lifeP;
+var Dir = 0;
 
 function setup() {
   createCanvas(600,600);
   s = new Snake();
-  frameRate(10);
-  //food = createVector(random(width), random(height));
+  pickLocation();
+  lifeP = createP();
 
 }
 
 function pickLocation() {
   var cols = floor(width/scl);
   var rows = floor(height/scl);
+  
   food = createVector(floor(random(cols)), floor(random(rows)));
   food.mult(scl);
+
 }
 
 function draw() {
   background(51);
+  frameRate(vel);
+  s.death();
   s.update();
   s.show();
 
-  s.eat(food);
+  if (s.eat(food)) {
+    pickLocation();
+  }
 
   fill(255, 0, 0);
   rect(food.x, food.y, scl, scl);
 }
 
 function keyPressed() {
+
   if (keyCode === UP_ARROW) {
-    s.dir(0, -1);
+    if (Dir !== 1) {
+      Dir = 2;
+      s.dir(0, -1);
+    }
+
   } else if (keyCode === DOWN_ARROW) {
-    s.dir(0, 1);
+    if (Dir !== 2) {
+      Dir = 1;
+      s.dir(0, 1);
+    }
+
   } else if (keyCode === LEFT_ARROW) {
-    s.dir(-1, 0);
+    if (Dir !== 3) {
+      Dir = 4;
+      s.dir(-1, 0);
+    }
+
   } else if (keyCode === RIGHT_ARROW) {
-    s.dir(1, 0);
+    if (Dir !== 4) {
+      Dir = 3;
+      s.dir(1, 0);
+    }
   }
 }
